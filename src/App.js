f0bff5c5985f3cb4466cd6e8af4d43ff47fdd1be
shekhar <shekhar.powar@outlook.com>
2023-07-26 19:26:31 +0530

    carousel added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@
 
 import './App.css';
 import Carousels from './component/Carousels';
-import Kidscomponent from './component/Kidscomponent';
-import Navbar from './component/Navbar';
+// import Kidscomponent from './component/Kidscomponent';
+// import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-   <Navbar/>
-   <Carousels/>
-   <Kidscomponent/>
+      <h1>testing</h1>
+      <Carousels/>
     </div>
   );
 }
