ee6c9516c87486392d9b7d5d837d0bb272f6a490
shekhar <shekhar.powar@outlook.com>
2023-07-26 19:28:00 +0530

    undo changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 
 import './App.css';
 import Carousels from './component/Carousels';
-// import Kidscomponent from './component/Kidscomponent';
-// import Navbar from './component/Navbar';
+import Kidscomponent from './component/Kidscomponent';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>testing</h1>
-      <Carousels/>
+   <Navbar/>
+   <Carousels/>
+   <Kidscomponent/>
     </div>
   );
 }
